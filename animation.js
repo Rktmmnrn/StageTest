a2d24d186f66e5ca4d7578559c9619f3fca02d54
rktmmnrn <rfanomezaniavo@gmail.com>
2024-12-16 12:18:38 +0300

    MAJ structure et JS 10%

--- animation.js
+++ animation.js
@@ -1,6 +1,14 @@
 function leftSlide() {
   let navbar = document.getElementById('nav-menu1'); 
+
   navbar.style.display = 'flex';
   navbar.style.transform = 'translateX(0)';
   navbar.style.transition = '500ms';
+}
+
+function rightSlide() {
+  let rightSect = document.getElementById('section-right');
+
+  rightSect.style.transform = 'translateX(0)';
+  rightSect.style.display = 'flex';
 }
